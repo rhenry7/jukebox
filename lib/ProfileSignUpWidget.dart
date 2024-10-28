0866956308a86fca432d4882d1e7c9854c870478
Ramone Henry <ramoneh94@gmail.com>
2024-10-28 15:06:58 -0400

    continue work on signup auth page

--- lib/ProfileSignUpWidget.dart
+++ lib/ProfileSignUpWidget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ionicons/ionicons.dart';
 
 class ProfileSignUp extends StatefulWidget {
   const ProfileSignUp({super.key});
@@ -28,11 +30,15 @@ class ProfileSignUpPage extends State<ProfileSignUp> {
                 children: [
                   // PROFILE_OVERVIEW
                   // SETTINGS
+                  Gap(30),
                   Row(
                     children: [
                       BackButton(),
                     ],
                   ),
+                  Gap(30),
+                  Text("Sign Up", style: TextStyle(fontSize: 20)),
+                  Gap(50),
                   TextFormField(
                     decoration: const InputDecoration(
                       icon: Icon(Icons.person),
@@ -52,9 +58,9 @@ class ProfileSignUpPage extends State<ProfileSignUp> {
                   ),
                   TextFormField(
                     decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      hintText: 'What do people call you?',
-                      labelText: 'Name *',
+                      icon: Icon(Ionicons.mail_outline),
+                      hintText: 'Enter your email',
+                      labelText: 'Email *',
                     ),
                     onSaved: (String? value) {
                       // This optional block of code can be used to run
@@ -68,9 +74,9 @@ class ProfileSignUpPage extends State<ProfileSignUp> {
                   ),
                   TextFormField(
                     decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      hintText: 'What do people call you?',
-                      labelText: 'Name *',
+                      icon: Icon(Ionicons.eye_outline),
+                      hintText: 'Password must be 8 characters long',
+                      labelText: 'Password *',
                     ),
                     onSaved: (String? value) {
                       // This optional block of code can be used to run
@@ -81,7 +87,83 @@ class ProfileSignUpPage extends State<ProfileSignUp> {
                           ? 'Do not use the @ char.'
                           : null;
                     },
-                  )
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      icon: Icon(Ionicons.eye_outline),
+                      hintText: 'Passwords must match',
+                      labelText: 'Confirm Password *',
+                    ),
+                    onSaved: (String? value) {
+                      // This optional block of code can be used to run
+                      // code when the user saves the form.
+                    },
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                  ),
+                  Gap(70),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action when the button is pressed
+                      // send to firebase
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(30.0), // Round radius
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
+                      backgroundColor: Colors.green[800], // Button color
+                    ),
+                    child: const Text(
+                      'Create Account',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Gap(50),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action when the button is pressed
+                      // send to firebase
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(30.0), // Round radius
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
+                      backgroundColor: Colors.blueAccent, // Button color
+                    ),
+                    child: const Text(
+                      'Sign Up With Google',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Gap(5),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action when the button is pressed
+                      // send to firebase
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(30.0), // Round radius
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 34, vertical: 12),
+                      backgroundColor: Colors.white, // Button color
+                    ),
+                    child: const Text(
+                      'Sign Up With Apple',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
                 ],
               ),
             ),
