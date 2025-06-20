30b241c4fbcf403823d82b270a76bb67ad558c8d
Ramone Henry <ramoneh94@gmail.com>
2025-06-20 12:28:09 -0400

    Clean up dialog and widget folder for comments and subcomments

---
+++ lib/ui/screens/feed/comments_responding_to_post.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_project/GIFs/gifs.dart';
+import 'package:flutter_test_project/models/review.dart';
+import 'package:ionicons/ionicons.dart';
+
+class CommentsRespondingToPost extends StatelessWidget {
+  final Future<List<Review>> comments;
+  const CommentsRespondingToPost({super.key, required this.comments});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: FutureBuilder<List<Review>>(
+                future: comments,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: snapshot.data!.length,
+                        physics:
+                            const NeverScrollableScrollPhysics(), // Disable
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          /**
+                                 * Because there is only one source for mock data for subcomments, the first subcomment user will show as the same user who made the initial review. To fix this, I am skipping that inital user in the line below. This is not a good solution, and can be approved by having additional mock users or real users.
+                                 */
+                          final comment = snapshot.data![index +
+                              1]; // TODO: REPLACE WHEN WE HAVE ACTUAL USERS
+                          //print(track);
+                          return Card(
+                            elevation: 0,
+                            margin: const EdgeInsets.all(5),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8)),
+                              side: BorderSide(
+                                  color: Color.fromARGB(56, 158, 158, 158)),
+                            ),
+
+                            //margin: const EdgeInsets.all(0),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(0.0),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        child: ListTile(
+                                          leading: const Icon(Ionicons
+                                              .person_circle_outline), // Fallback if no image is available,
+                                          title: Text(comment.displayName),
+                                          //subtitle: Text(comment.), use post time data
+                                        ),
+                                      ),
+                                      //Text(comment.comment),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    comment.review,
+                                    maxLines: 3,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      fontStyle: FontStyle.italic,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      // LIKES
+                                      Padding(
+                                        padding: const EdgeInsets.all(0),
+                                        child: Row(
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(0.0),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                children: [
+                                                  Row(
+                                                    children: [
+                                                      IconButton(
+                                                        icon: const Icon(
+                                                            Ionicons
+                                                                .heart_outline,
+                                                            color:
+                                                                Colors.white),
+                                                        onPressed: () {
+                                                          // Navigator.push(
+                                                          //     context,
+                                                          //     MaterialPageRoute(
+                                                          //         builder: (BuildContext
+                                                          //                 context) =>
+                                                          //             const SubComments()));
+                                                          // setState(() {
+                                                          //   "Liked!";
+                                                          //   Icons.thumb_up;
+                                                          // });
+                                                        },
+                                                      ),
+                                                      InkWell(
+                                                        onTap: () {
+                                                          print(
+                                                              "tapped inkwell, should route");
+                                                        },
+                                                        child: Text(
+                                                          comment.likes
+                                                              .toString(),
+                                                          style:
+                                                              const TextStyle(
+                                                                  color: Colors
+                                                                      .white),
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      // REPLIES
+                                      Padding(
+                                        padding: const EdgeInsets.all(0),
+                                        child: Row(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  right: 0),
+                                              child: Row(
+                                                children: [
+                                                  IconButton(
+                                                    icon: const Icon(
+                                                        Ionicons
+                                                            .chatbubble_outline,
+                                                        color: Colors.white),
+                                                    onPressed: () {
+                                                      // setState(() {
+                                                      //   "Liked!";
+                                                      //   Icons.thumb_up;
+                                                      // });
+                                                    },
+                                                  ),
+                                                  Text(
+                                                      comment.replies
+                                                          .toString(),
+                                                      style: const TextStyle(
+                                                          color: Colors.white)),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      // REPOSTS
+                                      Padding(
+                                        padding: const EdgeInsets.all(0),
+                                        child: Row(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.all(0),
+                                              child: Row(
+                                                children: [
+                                                  IconButton(
+                                                    icon: const Icon(
+                                                        Ionicons.repeat,
+                                                        color: Colors.white),
+                                                    onPressed: () {
+                                                      // thumbs up
+                                                    },
+                                                  ),
+                                                  Text(
+                                                      comment.reposts
+                                                          .toString(),
+                                                      style: const TextStyle(
+                                                          color: Colors.white)),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      // SHARES
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 4.0),
+                                        child: Row(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  right: 0),
+                                              child: Row(
+                                                children: [
+                                                  IconButton(
+                                                    icon: const Icon(
+                                                        Ionicons
+                                                            .paper_plane_outline,
+                                                        color: Colors.white),
+                                                    onPressed: () {
+                                                      // setState(() {
+                                                      //   "Liked!";
+                                                      //   Icons.thumb_up;
+                                                      // });
+                                                      // add state, update DB and UI to reflect change
+                                                    },
+                                                  ),
+                                                  Text(
+                                                      comment.reposts
+                                                          .toString(),
+                                                      style: const TextStyle(
+                                                          color: Colors.white)),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    print(snapshot);
+                    return Text('Error: ${snapshot.error}');
+                  }
+                  return const DiscoBallLoading();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/feed/user_profile_interaction_dialog.dart
@@ -0,0 +1,190 @@
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test_project/ui/screens/Profile/auth/following/follow.dart';
+import 'package:ionicons/ionicons.dart';
+
+class UserProfileInteractionDialog extends StatelessWidget {
+  final String displayName;
+  final int reviewCount;
+  final String accountCreationDate;
+  final VoidCallback? onClose;
+  String currentUid = FirebaseAuth.instance.currentUser!.uid;
+
+  UserProfileInteractionDialog({
+    Key? key,
+    required this.displayName,
+    required this.reviewCount,
+    required this.accountCreationDate,
+    this.onClose,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  border: Border.all(color: Colors.white, width: 2),
+                  color: Colors.black,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Profile Icon
+                    const Icon(
+                      Ionicons.person_circle,
+                      size: 60,
+                      color: Colors.blue,
+                    ),
+                    const SizedBox(height: 16),
+                    // Title
+                    Text(
+                      displayName,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    const SizedBox(height: 20),
+                    // User Name
+                    _buildInfoRow(
+                      icon: Ionicons.person,
+                      label: 'Name',
+                      value: displayName, // Replace with actual user name
+                    ),
+                    const SizedBox(height: 12),
+                    // Number of Reviews
+                    _buildInfoRow(
+                      icon: Ionicons.star,
+                      label: 'Reviews',
+                      value: reviewCount
+                          .toString(), // Replace with actual review count
+                    ),
+                    const SizedBox(height: 12),
+                    // Account Creation Date
+                    _buildInfoRow(
+                      icon: Ionicons.calendar,
+                      label: 'Member Since',
+                      value:
+                          'January 2023', // Replace with actual creation date
+                    ),
+                    const SizedBox(height: 24),
+                    // Close Button
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 12,
+                        ),
+                      ),
+                      child: const Text('Close'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        //Navigator.of(context).pop();
+                        print('Following user: $displayName as $currentUid');
+                        followUser(currentUid, displayName).then((_) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Followed successfully!'),
+                            ),
+                          );
+                        }).catchError((error) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Error following user: $error'),
+                            ),
+                          );
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 12,
+                        ),
+                      ),
+                      child: const Text('Follow'),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 3.0, right: 5.0),
+            child: Icon(Ionicons.person_circle_outline),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 3.0, right: 5.0),
+            child: Text(displayName),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+Widget _buildInfoRow({
+  required IconData icon,
+  required String label,
+  required String value,
+}) {
+  return Row(
+    children: [
+      Icon(
+        icon,
+        size: 20,
+        color: Colors.grey[600],
+      ),
+      const SizedBox(width: 12),
+      Text(
+        '$label: ',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: Colors.grey[700],
+        ),
+      ),
+      Expanded(
+        child: Text(
+          value,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          textAlign: TextAlign.end,
+        ),
+      ),
+    ],
+  );
+}
